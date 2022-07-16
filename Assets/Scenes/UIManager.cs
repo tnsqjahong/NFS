d89f7d739731263af883eec3b51346b9a10e7906
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2022-07-17 05:34:07 +0900

    .

---
+++ Assets/Scenes/UIManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.UI;
+
+public class UIManager : MonoBehaviour
+
+{
+    public GameObject ScrollArea;
+    bool hehmahopen=false;
+    // Start is called before the first frame update
+
+   void Start() {
+    ScrollArea.SetActive(false);
+   }
+    
+    public void hehmah()
+    {
+        if(hehmahopen==false){ScrollArea.SetActive(true); 
+        hehmahopen=true
+        ;}
+        else if(hehmahopen==true)
+        {ScrollArea.SetActive(false);
+        hehmahopen=false;}
+        
+        
+    }
+    
+
+}
